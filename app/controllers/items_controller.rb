7c48ae5e13131ddfdfbd67fc8abdf4e8ca2b9488
zradford <rad12009@byui.edu>
2020-05-09 19:59:08 -0700

    fix to new item error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -71,6 +71,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :description, :price, :quantity)
+      params.require(:item).permit(:name, :description, :price, :quantity, :currency)
     end
 end
