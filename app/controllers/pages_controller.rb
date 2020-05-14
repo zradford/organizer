661d8fb0a95f566258f29764d18269d6f3981bbb
zradford <rad12009@byui.edu>
2020-05-14 12:11:53 -0700

    authenticate user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
   def home
-    @user = User.find(current_user.id)
+    @user = User.find(current_user.id) if user_signed_in?
   end
 end
